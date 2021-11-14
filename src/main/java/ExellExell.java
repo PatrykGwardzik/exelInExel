67ad03ac347b96f1d9020208d5fb10dd34d54d8a
PatrykGwardzik <gwardzikpatryk@gmail.com>
2021-11-14 21:26:38 +0100

    fIRST

---
+++ src/main/java/ExellExell.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+
+
+
+public class ExellExell {
+
+        public static void main(String[] args) {
+
+           HSSFWorkbook nowyArkusz = new HSSFWorkbook();
+           nowyArkusz.getSheet("January");
+            System.out.println("HI");
+           
+
+
+        }
+}
